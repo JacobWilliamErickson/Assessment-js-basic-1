767dbfad9153db1b96057a940e2f9cc4bd007646
Jacob Erickson <Jakerson8@gmail.com>
2022-06-24 16:56:57 -0600

    Finalized Assessment

--- password.js
+++ password.js
@@ -1,14 +1,14 @@
-password = "jacoberickson88888"
+password = "jacoberickson1234"
 //Additional Checks - Check for maximum length (i.e. no more than 30 characters) && Check for forbidden words (“password”)
 // \d stands for all numbers between 0-9, /regular expressions/
 var numbers = /\d/
 // if statement to check password for forbidden words
 if (password.includes("password")||password.includes("1234")||password.includes("letmein")){
-    console.log("This password contains a hidden phrase '1234', 'password' or 'letmein' cannot be included in your password ")
+    console.log("This password contains a hidden phrase! '1234', 'password' or 'letmein' cannot be included in your password ")
     console.log(`d88PPPo ,8b.     88PPP. 
-    888ooo8 88'8o    88   8 
-    888   8 88PPY8.  88   8 
-    888PPPP 8b   Y'  88oop'`)
+888ooo8 88'8o    88   8 
+888   8 88PPY8.  88   8 
+888PPPP 8b   Y'  88oop'`)
 }
 //Nan is not a number, so if it is true it means the string contains some letter)
 //else if after the forbidden word check to make sure A:password length isn't too short, B password length isnt too long, C its contains atleast one number and D it contains atleast on Letter
@@ -20,7 +20,7 @@ d8b PPY8 888  888 888  888   88   8
 Y8PPPPPP 888oo888 888oo888   88oop' `)
 } //else if to give a password to short message 
 else if (password.length<10){
-    console.log(`Password is not long enough ${password.length}/10 required or does not contain both numbers and letters`)
+    console.log(`Password is either not long enough ${password.length}/10 required or does not contain both numbers and letters`)
     console.log(`d88PPPo ,8b.     88PPP. 
 888ooo8 88'8o    88   8 
 888   8 88PPY8.  88   8 
@@ -28,7 +28,7 @@ else if (password.length<10){
 } 
 //else statement to give the password is too long message
 else{
-    console.log(`Password is too long ${password.length}/30 maximum or does not contain both numbers and letters`)
+    console.log(`Password is either too long ${password.length}/30 maximum or does not contain both numbers and letters`)
     console.log(`d88PPPo ,8b.     88PPP. 
 888ooo8 88'8o    88   8 
 888   8 88PPY8.  88   8 
